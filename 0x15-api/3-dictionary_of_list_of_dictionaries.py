dbe60c502df4310b5ce5dd19a30a6ba1887f5690
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-02-20 21:35:20 +0200

    Adding the last task

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,21 +7,21 @@ if __name__ == '__main__':
     response = requests.get(
         f'https://jsonplaceholder.typicode.com/users/'
     )
-    with open('todo_all_employees.json', 'w') as file:
-        json_string = {}
-        for user in response.json():
-            USER_ID = user['id']
-            USERNAME = user['username']
-            tasks_response = requests.get(
-                f'https://jsonplaceholder.typicode.com/todos/?userId={USER_ID}'
-            )
+    json_string = {}
+    for user in response.json():
+        USER_ID = user['id']
+        USERNAME = user['username']
+        tasks_response = requests.get(
+            f'https://jsonplaceholder.typicode.com/todos/?userId={USER_ID}'
+        )
 
-            json_string[str(USER_ID)] = [
-                {
-                    "username": USERNAME,
-                    "task": dictionary['title'],
-                    "completed": dictionary['completed'],
-                }
-                for dictionary in tasks_response.json()
-            ]
-            file.write(json.dumps(json_string))
+        json_string[str(USER_ID)] = [
+            {
+                "username": USERNAME,
+                "task": dictionary['title'],
+                "completed": dictionary['completed'],
+            }
+            for dictionary in tasks_response.json()
+        ]
+    with open('todo_all_employees.json', 'w') as file:
+        file.write(json.dumps(json_string))
