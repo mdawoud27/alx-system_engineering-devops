7a7e0f843937ece81f5786d7f9afe29fd9f9b44f
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-02-20 18:20:41 +0200

    Adding the first task

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""script that, using REST API, for a given employee ID,
+returns information about his/her Todo list progress."""
+if __name__ == '__main__':
+    import sys
+    import requests
+
+    if len(sys.argv) < 2:
+        exit()
+    else:
+        employee_id = sys.argv[1]
+
+    response = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    )
+    EMPLOYEE_NAME = response.json()['name']
+    tasks_response = requests.get(
+        f'https://jsonplaceholder.typicode.com/todos/?userId={employee_id}'
+    )
+    TOTAL_NUMBER_OF_TASKS = len(tasks_response.json())
+    NUMBER_OF_DONE_TASKS = 0
+    TASK_TITLE = ""
+    for dictionary in tasks_response.json():
+        for key, value in dictionary.items():
+            if key == 'completed' and value is True:
+                NUMBER_OF_DONE_TASKS += 1
+                TASK_TITLE += '\n'
+                TASK_TITLE += '\t'
+                TASK_TITLE += dictionary['title']
+
+    print(
+        f'Employee {EMPLOYEE_NAME} is done with tasks({NUMBER_OF_DONE_TASKS}/'
+        f'{TOTAL_NUMBER_OF_TASKS}):', end=""
+    )
+    print(TASK_TITLE)
