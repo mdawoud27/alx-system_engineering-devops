29ff8f2333b8daee48f4c9eff5d0f709fdbd66f2
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-06-30 10:11:03 +0300

    Update the file

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
-# User limit task
+# User Limit Task.
 
-exec { 'increasing hard file':
-  command => 'sed -i "/holberton hard/s/5/4096" /etc/security/limits.conf',
+exec { 'increase-hard-file':
+  command => 'sed -i "/holberton hard/s/5/4096/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
-exec { 'increasing soft file':
-  command => 'sed -i "/holberton soft/s/4/4096" /etc/security/limits.conf',
+exec { 'increase-soft-file':
+  command => 'sed -i "/holberton soft/s/4/4096/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
