254bd599fd52a8200f93b84606bc4f481df8467c
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-06-30 21:25:09 +0300

    Adding 1-top_ten.py file

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""""""
+import requests
+
+
+def top_ten(subreddit):
+    """function that queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit."""
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    headers = {'user-Agent': 'Too Many Requests'}
+
+    response = requests.get(url, headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+        return
+
+    data = response.json()
+    posts = data.get('data', {}).get('children', [])
+
+    for post in posts:
+        print(post.get('data', {}).get('title', None))
