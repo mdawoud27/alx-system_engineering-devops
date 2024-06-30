6e1cefac5817363a8a2eb6759c1ac59028eb091c
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-06-30 21:02:49 +0300

    Adding 0-subs.py file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,12 +4,16 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """function that queries the Reddit API and
+    """Function that queries the Reddit API and
     returns the number of subscribers"""
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'user-Agent': 'Too Many Requests'}
 
-    request = requests.get(url=url, headers=headers).json()
-    subscribers = request.get('data', {}).get('subscribers', 0)
+    response = requests.get(url=url, headers=headers)
+    if response.status_code != 200:
+        return 0
+
+    data = response.json()
+    subscribers = data.get('data', {}).get('subscribers', 0)
 
     return subscribers
