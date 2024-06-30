d9e915eb40c73bb952e3cc6f953075a2287b4392
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-06-30 21:27:22 +0300

    Adding 1-top_ten.py file

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,7 +5,7 @@ import requests
 
 
 def top_ten(subreddit):
-    """function that queries the Reddit API and prints the titles
+    """Function that queries the Reddit API and prints the titles
     of the first 10 hot posts listed for a given subreddit."""
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
     headers = {'user-Agent': 'Too Many Requests'}
